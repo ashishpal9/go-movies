842cbd8178007815824bc02e7c044795c1eed876
ashlar7860901 <ashlar.pal@gmail.com>
2025-07-01 11:44:47 +0530

    new func added

--- main.go
+++ main.go
@@ -30,10 +30,36 @@ func getMovies(w http.ResponseWriter , r*http.Request){
 	json.NewEncoder(w).Encode(movies)
 }
 
-func deleteMovie(w http.ResponseWriter , r*http.Request){
-	w.Hander().Ser("Content-Type", "application/json")
-	params := 
+func getMovie(w http.ResponseWriter , r*http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range movies{
+		if item.ID == params["ID"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
 }
+
+func deleteMovie(w http.ResponseWriter , r*http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index , item := range movies{
+		if item.ID == params["ID"]{
+			movies == append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func createMovie(w http.ResponseWriter, r*http.Request){
+	w.Header()
+}
+
+
 
 func main(){
 	r := mux.NewRouter()
